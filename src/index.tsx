a162d9a9ca9fef683e6bb59581293b676b8f4c36
w l <wl@wdeMac-mini.local>
2024-07-14 20:22:44 +0800

    feat: log

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 import Router from "./router";
 import i18n from "./translation";
@@ -10,6 +13,8 @@ const App = () => (
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
       <Router />
+      <Analytics />
+      <SpeedInsights />
     </I18nextProvider>
   </BrowserRouter>
 );
